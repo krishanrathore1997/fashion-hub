b281657dad1ea8ba4af4269a74f041e9dac2b159
krishanrathore1997 <krishanrathore8888@gmail.com>
2024-06-05 08:03:12 +0530

    slider add

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,6 +48,42 @@
             ]
         });
     }); 
+
+	$(document).ready(function(){
+        $('.slider').slick({
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 2000,
+            arrows: false,
+            dots: false,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 1,
+                        infinite: true,
+                        dots: true
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+            ]
+        });
+    });
 	$('.count').each(function () {
 		$(this).prop('Counter',0).animate({
 			Counter: $(this).text()
